eac20deb4243900b9a671895e3a0c98d0f863fd5
conk <18170872531@163.com>
2021-05-18 15:42:18 +0800

    用go实现冒泡排序

---
+++ 01-排序算法/bubblesort.go
@@ -0,0 +1,53 @@
+/*
+Author: Conk
+Function: select_sort
+CreateTime: 2021-05-18 00:10:00
+UpdateTime: 2021-05-18 00:10:00
+01排序算法-冒泡排序:
+比较相邻的元素，如果前一个比后一个大，就把它们两个调换位置
+5 4 3 2 1
+第一轮: 5
+5 > 4 : 4 5 3 2 1
+5 > 3 : 4 3 5 2 1
+5 > 2 : 4 3 2 5 1
+5 > 1 : 4 3 2 1 5
+第二轮: 4
+4 > 3 : 3 4 2 1 5
+4 > 2 : 3 2 4 1 5
+4 > 1 : 3 2 1 4 5
+以此类推....
+
+平均时间复杂度: O(n^2)
+空间复杂度: 1
+稳定性: 稳定
+*/
+
+
+package main
+
+import "fmt"
+
+func BubbleSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := i+1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[j], arr[i] = arr[i], arr[j]
+			}
+		}
+	}
+}
+
+
+func main() {
+	testData01 := []int{1, 2, 3, 4, 5}
+	BubbleSort(testData01)
+	fmt.Println(testData01)
+
+	testData02 := []int{5, 4, 3, 2, 1}
+	BubbleSort(testData02)
+	fmt.Println(testData02)
+
+	testData03 := []int{5, 1, 2, 2, 3, 4, 4}
+	BubbleSort(testData03)
+	fmt.Println(testData03)
+}
